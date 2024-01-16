ecdf2e78b4dddece6ea51dd2094e60f5f6e24c2d
Rakibul-98 <rakibul.rupom2001@gmail.com>
2024-01-16 19:48:45 +0600

    navbar added

--- src/Home/Components/NavBar/NavBar.js
+++ src/Home/Components/NavBar/NavBar.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div>
-            
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl text-[#072B6B]" href='/'><img className='w-6 h-6 mt-2 -me-1.5' src="https://i.ibb.co/NyGRRnx/logo.png" alt="" /> Hygge<span className='font-bold -ms-2'>X</span></a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='#flashcard'>Flashcard</a></li>
+                    <li><a href='#contact'>Contact</a></li>
+                    <li><a href='#faq'>FAQ</a></li>
+                    <button class="bg-gradient-to-b from-[#072B6B] to-blue-700 hover:bg-blue-700 text-white py-2 px-4 rounded-full">
+                        <a href="#login">Login</a>
+                    </button>
+                </ul>
+            </div>
         </div>
     );
 };
