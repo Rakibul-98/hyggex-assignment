f9f56e1e79a66d6c70f86b9c78f3c445e5fee499
Rakibul-98 <rakibul.rupom2001@gmail.com>
2024-01-16 20:53:06 +0600

    breadcrumbs added

--- src/Home/Components/BreadCrumbs/BreadCrumbs.js
+++ src/Home/Components/BreadCrumbs/BreadCrumbs.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const BreadCrumbs = () => {
     return (
-        <div>
-            
+        <div className="mx-7 text-sm breadcrumbs">
+            <ul>
+                <li>
+                    <a href='/'>
+                        <img className='w-3' src="https://i.ibb.co/9bw39Dd/home-1392-svgrepo-com.png" alt="" />
+                    </a>
+                </li>
+                <li><a href='#card'>Flashcard</a></li>
+                <li><a href='#math'>Mathematics</a></li>
+                <li className='font-semibold text-[#0A2B65]'>Add Document</li>
+            </ul>
         </div>
     );
 };
--- src/Home/Components/Home.js
+++ src/Home/Components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar/NavBar';
+import BreadCrumbs from './BreadCrumbs/BreadCrumbs';
 
 const Home = () => {
     return (
         <div>
             <NavBar/>
+            <BreadCrumbs/>
         </div>
     );
 };
