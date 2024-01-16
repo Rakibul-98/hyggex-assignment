e5eeb8028c220be6d9f04888b72c2d666c03aaa4
Rakibul-98 <rakibul.rupom2001@gmail.com>
2024-01-17 01:31:04 +0600

    faq section added

--- src/Home/Components/FAQ/FAQ.js
+++ src/Home/Components/FAQ/FAQ.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
 const FAQ = () => {
+
+    const queries = [
+        {
+            id:1, ques:"Can education flashcards be used for all age groups?", ans:"Education flashcards are versatile tools suitable for all ages. Whether featuring colorful images for younger learners or complex concepts for older ones, flashcards can adapt to various educational needs."
+        },
+        {
+            id:2, ques:"How do education flashcards work?", ans:"Education flashcards work by presenting concise information or questions on one side and corresponding answers on the other. Learners use them for active recall, reinforcing knowledge through repetition and visual cues."
+        },
+        {
+            id:3, ques:"Can education flashcards be used for test preparation?", ans:"Yes, education flashcards are effective tools for test preparation. They facilitate active recall, aid in memorization, and allow learners to review key concepts quickly. Flashcards are particularly useful for reinforcing vocabulary, formulas, historical dates, and other essential information."
+        },
+        
+    ]
     return (
-        <div>
-            
+        <div className='py-16'>
+            <h2 className='text-5xl font-bold text-[#072B6B] pb-2'>FAQ</h2>
+            <div className='w-8/12'>
+                {
+                    queries.map((faq)=>
+                    <div key={faq.id} x-data="{ open: false }" className="collapse collapse-arrow border border-[#072B6B] my-5">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-lg font-medium">
+                        {faq.ques}
+                    </div>
+                    <div className="collapse-content">
+                        <p>{faq.ans} </p>
+                    </div>
+                </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
--- src/Home/Components/Main/Main.js
+++ src/Home/Components/Main/Main.js
@@ -7,7 +7,20 @@ const Main = () => {
             <div className="header mt-8 mb-5">
                 <h3 className='font-bold text-[#072B6B] text-3xl'>Relations and Functions ( Mathematics )</h3>
             </div>
-            <Banner/>
+            <Banner />
+            <div className='flex justify-between items-center'>
+                <div className='flex'>
+                    <img className='h-[80px] w-[80px] p-3 rounded-full border shadow-xl me-2' src="https://i.ibb.co/NyGRRnx/logo.png" alt="" />
+                    <div>
+                        <p>Published by</p>
+                        <p className='text-4xl text-[#072B6B]'>Hygge<span className='font-bold'>X</span></p>
+                    </div>
+                </div>
+                <div className='flex items-center py-16'>
+                    <svg className='w-12 me-1' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <rect width="24" height="24" fill="white"></rect> <path fill-rule="evenodd" clip-rule="evenodd" d="M13 9C13 8.44772 12.5523 8 12 8C11.4477 8 11 8.44772 11 9V11H9C8.44772 11 8 11.4477 8 12C8 12.5523 8.44772 13 9 13H11V15C11 15.5523 11.4477 16 12 16C12.5523 16 13 15.5523 13 15V13H15C15.5523 13 16 12.5523 16 12C16 11.4477 15.5523 11 15 11H13V9ZM2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12Z" fill="#072B6B"></path> </g></svg>
+                    <p className='text-[34px]'>Create Flashcard</p>
+                </div>
+            </div>
         </div>
     );
 };
